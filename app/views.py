69bc95e0ad028eb93c10109584d9700c8f95f87a
= <nfazzio@gmail.com>
2014-06-02 17:25:47 -0500

    Completed pt2

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = { 'nickname': 'Nick' }
+    posts = [ #fake array of posts
+        {
+            'author': { 'nickname': 'Jimmy' },
+            'body': 'I\'m studying for step1!'
+        },
+        {
+            'author': { 'nickname': 'Jo' },
+            'body': 'Did anyone else watch True Detective?'
+        }
+    ]
+    return render_template("index.html",
+                           title = 'HOME',
+                           user = user,
+                           posts = posts)
